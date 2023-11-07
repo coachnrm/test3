246b902b342842a48c4a09f061b1badda4e16643
Coach <nuttapong_coach@hotmail.com>
2023-11-07 14:49:47 +0700

    อัปลงครั้งที่ 1

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+const mystyle = {
+  color: "white",
+  backgroundColor: "DodgerBlue",
+  padding: "10px",
+  fontFamily: "Arial"
+};
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <h1 style={mystyle}>hello world</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
